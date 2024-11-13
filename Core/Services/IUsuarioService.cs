bcd497565488af940b9bba57ec14db8d35d155ee
colombo2023 <127800159+colombo2023@users.noreply.github.com>
2024-11-13 19:32:49 -0300

    Add files via upload
    Adicionando nova pasta

---
+++ Core/Services/IUsuarioService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Core.Models;
+
+namespace Core.Services {
+    public interface IUsuarioService {
+        Task<UsuarioModels> RegistrarUsuario(UsuarioModels novoUsuario);
+        Task<UsuarioModels> GetUsuarioOrThrowException(string usuarioId);
+        Task<UsuarioModels?> GetUsuario(string usuarioId);
+    }
+}
