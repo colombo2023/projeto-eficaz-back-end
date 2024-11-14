6ce852bba2a069bbeb4a2799f296c8e73892420e
DAKOTAPARTS_\carloscolombo <carlos.colombo@connectparts.com.br>
2024-11-14 14:57:34 -0300

    Alterado de nulo pra vazio

--- Usuarios_cadastro/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Usuarios_cadastro/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcd497565488af940b9bba57ec14db8d35d155ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
